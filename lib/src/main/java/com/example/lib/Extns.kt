c17622f651481f5a7969879bded9909963c62e30
Deepak <deepak.sahu@enveu.com>
2023-06-20 06:42:06 +0530

    example of extension func

--- lib/src/main/java/com/example/lib/Extns.kt
+++ lib/src/main/java/com/example/lib/Extns.kt
@@ -1,16 +1,32 @@
 package com.example.lib
 
-import java.lang.Integer.sum
+import java.util.Locale
 
 
 fun main() {
     fun <T> List<T>.secondLast(): T {
         return this[size - 2]
     }
-
     val list = listOf(1, 2, 3, 4, 5)
     val secondLast = list.secondLast() // returns 4
-//    android.util.Log.d(TAG, "main: $secondLast")
-}
+    println(secondLast)
 
+    println("#############################################################")
+
+    val num = 5
+    fun Int.square(): Int {
+        return this * this
+    }
+    val sqaureValue = num.square()
+    println(sqaureValue)
+
+    println("#############################################################")
+
+    val text = "Hello, World"
+    fun String.containsIgnore(value: String): Boolean {
+        return this.lowercase(Locale.getDefault()).contains(value.lowercase(Locale.getDefault()))
+    }
+    val containSubstring = text.containsIgnore("Baby")
+    println(containSubstring)
+}
 
