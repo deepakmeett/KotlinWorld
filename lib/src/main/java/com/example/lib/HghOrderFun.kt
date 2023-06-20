07774947caef2bbb3024ec5c9f93b4c99c7aedd8
Deepak <deepak.sahu@enveu.com>
2023-06-20 07:19:51 +0530

    High order function done

---
+++ lib/src/main/java/com/example/lib/HghOrderFun.kt
@@ -0,0 +1,22 @@
+package com.example.lib
+
+fun main() {
+    val result1 = orderHighFun(2, 3, ::sum)
+    println("Result of sum: $result1") // Output: Result of sum: 5
+
+    val result2 = orderHighFun(3, 4, ::multiply)
+    println("Result of multiply: $result2") // Output: Result of multiple: 12
+}
+
+fun sum(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun multiply(a: Int, b: Int): Int {
+    return a * b
+}
+
+fun orderHighFun(a: Int, b: Int, anyFun: (Int, Int) -> Int): Int {
+    return anyFun(a, b)
+}
+
