42025f50868815b2a4decf95f5c7d0e303119f96
Deepak <deepak.sahu@enveu.com>
2023-09-02 06:31:07 +0530

    find the missing number done

--- lib/src/main/java/com/example/lib/simple_programe/findTheMissingNum.kt
+++ lib/src/main/java/com/example/lib/simple_programe/findTheMissingNum.kt
@@ -3,21 +3,19 @@ package com.example.lib.simple_programe
 fun main() {
     var firstElement = 0
     var secondElement = 0
-    var bwElement = 0
-    val list = listOf(1, 3, 5, 7, 9, 10)
+    val list = listOf(1, 7, 9, 10)
+
     for (i in list.indices) {
         if (i < list.size - 1) {
             firstElement = list[i]
-            bwElement = firstElement + 1
         }
         if (i + 1 < list.size) {
             secondElement = list[i + 1]
         }
-
-        if (bwElement in (firstElement + 1) until secondElement){
-            println("Missing number is -> $bwElement")
+        for (j in firstElement..secondElement) {
+            if (j > firstElement && j < secondElement) {
+                println("Missing number is -> $j")
+            }
         }
-
-        bwElement = 0
     }
 }
