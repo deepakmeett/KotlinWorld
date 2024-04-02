f9e3cd65f8b0646d39dec5c9462b9de00adecfbf
Deepak.Sahu <deepaktech671@gmail.com>
2024-04-02 11:09:16 +0530

    Triple Quote done

---
+++ lib/src/main/java/com/example/lib/kotlin_concept/TripleQuotes.kt
@@ -0,0 +1,12 @@
+package com.example.lib.kotlin_concept
+
+fun main() {
+    val message: String = """
+        Hello
+        Baby
+        You look sexy
+        I am sexy
+        Why don't we go somewhere
+    """.trimIndent()
+    println(message)
+}
