8b0b691869677432d7f15431983d9bf558254654
Deepak.Sahu <deepaktech671@gmail.com>
2025-03-09 15:35:07 +0530

    Factorial code added

--- lib/src/main/java/com/example/lib/simple_programe/Factorial.kt
+++ lib/src/main/java/com/example/lib/simple_programe/Factorial.kt
@@ -3,21 +3,19 @@ package com.example.lib.simple_programe
 
 
 fun main() {
+    val num = 5
     var v1 = 1
-    for (i in 1..5) {
+    for (i in 1..num) {
         v1 *= i
     }
     println(v1)
 
-    val list = listOf(1, 2, 3, 4, 5, 6)
     var j: Int
-    var l = 1
-    for (i in list.indices) {
-        if (list.size > i + 1) {
-            j = l
-            l = j * list[i + 1]
-        }
+    var m = 1
+    for (i in 1..num) {
+            j = m
+            m = j * i
     }
-    println(l)
+    println(m)
 
 }
