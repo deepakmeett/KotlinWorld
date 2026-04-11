3611c6e0d71ea6fe41d86a258b7f8ae9f8172071
deepakSahu <deepaktech671@gmail.com>
2026-04-11 11:55:29 +0530

    Palindrome check algorithm implementation

--- lib/src/main/java/com/example/lib/simple_programe/WordPalidrome.kt
+++ lib/src/main/java/com/example/lib/simple_programe/WordPalidrome.kt
@@ -1,24 +1,22 @@
 package com.example.lib.simple_programe
 
-
 fun main() {
-    val name = "the cat and the dog"
-    val mm = mutableMapOf<String, Int>()
-    val ml = mutableListOf<String>()
-    var word = ""
-    for (i in name) {
-        if (i.toString() != " ") {
-            word += i
-        } else {
-            print(word)
-            ml.add(word)
-            word = ""
-        }
+    val name = "racecar"
+    println(isPalindrome(name))
+
+
+    fun isPalindrome(name: String): Boolean {
+     var left = 0
+     var right = name.length - 1
+     while(left < right){
+         if(name[left] != name[right]){
+             return false
+         }
+         left++
+         right--
+     }
+     return true
+        //Kotlin collections used.
+//        return name == name.reversed()
     }
-//
-    ml.add(word)
-    for (i in ml) {
-        mm[i] = (mm[i] ?: 0) + 1
-    }
-    print(mm)
 }
