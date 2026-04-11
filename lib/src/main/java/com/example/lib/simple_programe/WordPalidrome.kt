c70c5555905230f01bbe8301ca00e80a64046307
deepakSahu <deepaktech671@gmail.com>
2026-04-11 12:41:21 +0530

    Refactor WordPalidrome.kt and move isPalindrome function outside main function

--- lib/src/main/java/com/example/lib/simple_programe/WordPalidrome.kt
+++ lib/src/main/java/com/example/lib/simple_programe/WordPalidrome.kt
@@ -3,20 +3,19 @@ package com.example.lib.simple_programe
 fun main() {
     val name = "racecar"
     println(isPalindrome(name))
-
-
-    fun isPalindrome(name: String): Boolean {
-     var left = 0
-     var right = name.length - 1
-     while(left < right){
-         if(name[left] != name[right]){
-             return false
-         }
-         left++
-         right--
-     }
-     return true
-        //Kotlin collections used.
-//        return name == name.reversed()
+}
+fun isPalindrome(name: String): Boolean {
+    var left = 0
+    var right = name.length - 1
+    while(left < right){
+        if(name[left] != name[right]){
+            return false
+        }
+        left++
+        right--
     }
+    return true
+
+    //Kotlin collections used.
+//        return name == name.reversed()
 }
