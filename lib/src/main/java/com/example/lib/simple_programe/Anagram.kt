9958401b720ae8ead9553d896ff4437b8978b880
deepakSahu <deepaktech671@gmail.com>
2026-04-13 22:05:38 +0530

    Anagram algorithm implementation in Kotlin.

---
+++ lib/src/main/java/com/example/lib/simple_programe/Anagram.kt
@@ -0,0 +1,28 @@
+package com.example.lib.simple_programe
+
+fun main() {
+    val s = "anagram"
+    val t = "nagaram"
+    println(isAnagram(s, t))
+}
+
+fun isAnagram(s: String, t: String): Boolean {
+    if (s.length != t.length) return false
+
+    val map = mutableMapOf<Char, Int>()
+
+    for (c in s) {
+        map[c] = map.getOrDefault(c, 0) + 1
+    }
+
+    for (c in t) {
+        if (!map.containsKey(c)) return false
+
+        map[c] = map[c]!! - 1
+        if (map[c] == 0) {
+            map.remove(c)
+        }
+    }
+
+    return map.isEmpty()
+}
