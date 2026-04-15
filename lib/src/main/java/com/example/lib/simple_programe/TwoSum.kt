41498da4c8a3ba59b9a9231be31ad71f5a9adc35
deepakSahu <deepaktech671@gmail.com>
2026-04-15 09:11:16 +0530

    Refactor TwoSum implementation to use a HashMap for better performance

--- lib/src/main/java/com/example/lib/simple_programe/TwoSum.kt
+++ lib/src/main/java/com/example/lib/simple_programe/TwoSum.kt
@@ -7,13 +7,24 @@ fun main() {
     println(result.toList())
 }
 
-fun twoSum(myArray: IntArray, target: Int): IntArray {
-    for (i in myArray.indices) {
-        for (j in i + 1 until myArray.size) {
-            if (myArray[i] + myArray[j] == target) {
-                return intArrayOf(i, j)
-            }
+//fun twoSum(myArray: IntArray, target: Int): IntArray {
+//    for (i in myArray.indices) {
+//        for (j in i + 1 until myArray.size) {
+//            if (myArray[i] + myArray[j] == target) {
+//                return intArrayOf(i, j)
+//            }
+//        }
+//    }
+//    return intArrayOf(-1, -1)
+//}
+fun twoSum(arr: IntArray, target: Int): IntArray{
+    val map = mutableMapOf<Int, Int>()
+    for(i in arr.indices){
+        val current = target - arr[i]
+        if(map.containsKey(current)){
+            return intArrayOf(map[current]!!, i)
         }
+        map[arr[i]] = i
     }
     return intArrayOf(-1, -1)
 }
