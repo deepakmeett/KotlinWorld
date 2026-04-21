0db2400c69ba644177babbe4db5c6e01742eb689
deepakSahu <deepaktech671@gmail.com>
2026-04-21 09:29:19 +0530

    Refactor Anagram implementation for improved null safety and code cleanup

--- lib/src/main/java/com/example/lib/simple_programe/Anagram.kt
+++ lib/src/main/java/com/example/lib/simple_programe/Anagram.kt
@@ -5,24 +5,20 @@ fun main() {
     val t = "nagaram"
     println(isAnagram(s, t))
 }
-
 fun isAnagram(s: String, t: String): Boolean {
     if (s.length != t.length) return false
-
     val map = mutableMapOf<Char, Int>()
-
     for (c in s) {
         map[c] = map.getOrDefault(c, 0) + 1
     }
-
     for (c in t) {
         if (!map.containsKey(c)) return false
 
-        map[c] = map[c]!! - 1
+        val count = map[c] ?: return false // Get count or return false if null
+        map[c] = count - 1
         if (map[c] == 0) {
             map.remove(c)
         }
     }
-
     return map.isEmpty()
 }
