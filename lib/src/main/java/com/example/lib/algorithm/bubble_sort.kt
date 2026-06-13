71189f784fcc71a0d3aefbe5ab6439436b244d4a
DeepakSahu <deepakdm17092210@gmail.com>
2026-06-13 22:49:21 +0530

    Update bubble_sort.kt to use array.indices

--- lib/src/main/java/com/example/lib/algorithm/bubble_sort.kt
+++ lib/src/main/java/com/example/lib/algorithm/bubble_sort.kt
@@ -4,7 +4,7 @@ package com.example.lib.algorithm
 fun main() {
     val array = arrayOf(5, 4, 3, 2, 1)
 
-    for (i in 0 until array.size) {
+    for (i in array.indices) {
         for (j in 0 until array.size - i - 1) {
             if (array[j] > array[j + 1]) {
                 val temp = array[j]
