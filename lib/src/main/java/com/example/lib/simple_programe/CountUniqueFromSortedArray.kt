ff7f0ddd5ee1577faa2742b8c2b036f876d689af
DeepakSahu <deepakdm17092210@gmail.com>
2026-06-16 22:46:12 +0530

    Rename RemoveDuplicatesFromSortedArray.kt to CountUniqueFromSortedArray.kt and update function name to countUnique

--- lib/src/main/java/com/example/lib/simple_programe/CountUniqueFromSortedArray.kt
+++ lib/src/main/java/com/example/lib/simple_programe/CountUniqueFromSortedArray.kt
@@ -4,11 +4,11 @@ fun main() {
     val nums = intArrayOf(0, 0, 1, 1, 1, 2, 2, 3, 3, 3, 4, 5)
 //    val nums = intArrayOf(1, 1, 2)
     println(
-        removeDuplicate(nums)
+        countUnique(nums)
     )
 }
 
-fun removeDuplicate(nums: IntArray): Int {
+fun countUnique(nums: IntArray): Int {
     var res = 0
     for (i in 0 until nums.size) {
         if (i < nums.size - 1 && nums[i] == nums[i + 1]) {
