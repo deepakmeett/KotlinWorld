ceb0f5514cc62b5ea793a6bb52c0645ae2489f24
DeepakSahu <deepakdm17092210@gmail.com>
2026-07-07 23:44:18 +0530

    Add longestBalancedSubarray algorithm implementation in Kotlin

---
+++ lib/src/main/java/com/example/lib/android_related/longestBalancedSubarray.kt
@@ -0,0 +1,30 @@
+package com.example.lib.android_related
+
+fun main() {
+    val nums = intArrayOf(2, 5, 4, 3)
+//    val nums = intArrayOf(3,2,2,5,4)
+//    val nums = intArrayOf(1, 2, 3, 2)
+    println(longestBalancedSubstring(nums))
+}
+
+fun longestBalancedSubstring(nums: IntArray): Int {
+    var ans = 0
+
+    for (i in nums.indices) {
+        val evenSet = mutableSetOf<Int>()
+        val oddSet = mutableSetOf<Int>()
+
+        for (j in i until nums.size) {
+
+            if (nums[j] % 2 == 0) evenSet.add(nums[j]) else oddSet.add(nums[j])
+
+            // Check if both sets have the same size
+            if (evenSet.size == oddSet.size) {
+                val total = j - i + 1
+                if (ans < total) ans = total
+            }
+        }
+    }
+
+    return ans
+}
